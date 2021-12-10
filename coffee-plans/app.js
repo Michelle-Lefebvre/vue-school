2b32eea9263b97c2c6f001453f1609f6ef4d159b
Michelle <michelle.lefebvre@valtech.com>
2021-12-10 08:56:55 -0500

    Nested Components

--- coffee-plans/app.js
+++ coffee-plans/app.js
@@ -1,12 +1,19 @@
 Vue.createApp({
+
+})
+
+.component('plan', {
+    template: '#plan-template',
+    props:['name']
+})
+
+.component('plan-picker', {
+    template: '#plan-picker-template',
     data() {
         return {
             plans: ['The Single', 'The Curious', 'The Addict']
         }
     }
 })
-.component('plan', {
-    template: '#plan-template',
-    props:['name']
-})
+
 .mount('#app')
