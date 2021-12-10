25d25b9b84159ceb6c02afbfdd2eccf9f3b876af
Michelle <michelle.lefebvre@valtech.com>
2021-12-09 19:29:48 -0500

    Vue.js 3 Components Fundamentals: coffee plans reusable components

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # vue-school
 ##  Vue.js 3 Fundamentals course
 [Shopping List](https://codepen.io/mmlvlt/full/OJxXKvg)
+[Counter](https://codepen.io/mmlvlt/full/YzrGKYE) Components
+[Coffee Plans](https://codepen.io/mmlvlt/full/JjrRPpW)
---
+++ coffee-plans/app.js
@@ -0,0 +1,12 @@
+Vue.createApp({
+    data() {
+        return {
+            plans: ['The Single', 'The Curious', 'The Addict']
+        }
+    }
+})
+.component('plan', {
+    template: '#plan-template',
+    props:['name']
+})
+.mount('#app')
